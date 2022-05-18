ff56ed0e8d8c53e6a7cfee007296e1a352f009a2
Zuriel <2001555@sit.singaporetech.edu.sg>
2022-05-18 17:49:10 +0800

    different initialising method

--- In_C/main.c
+++ In_C/main.c
@@ -13,19 +13,21 @@ struct date{
     int year;
 };
 /* Structure variables */
-struct date today, tomorrow, yesterday;
+struct date today, tomorrow, yesterday, holiday;
 
 
 
 int main(){
     /* Initializing Global Variables */
-    today.month = 5; today.day = 18; today.year = 22;
-    tomorrow.month = 5; tomorrow.day= 6; tomorrow.year = 22;
-    yesterday.month = 5; yesterday.day = 4; yesterday.year = 22;
+    today.month = 5; today.day = 18; today.year = 22; // today 
+    tomorrow.month = 5; tomorrow.day= 6; tomorrow.year = 22; // tomorrow
+    yesterday.month = 5; yesterday.day = 4; yesterday.year = 22; // yesterday
+    holiday = (struct date){5, 10, 22};
 
     printf("today is %d/%d/%d\n", today.day, today.month, today.year);
     printf("Yesterday is %d/%d/%d\n", yesterday.day, yesterday.month, yesterday.year);
     printf("tomorrow is %d/%d/%d\n", tomorrow.day, tomorrow.month, tomorrow.year);
+    printf("holiday is %d/%d/%d\n", holiday.day, holiday.month, holiday.year);
 
     return 0;
 }
